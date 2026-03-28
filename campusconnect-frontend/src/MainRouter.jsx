8211b849a3bac0545f0a31517f7a7c80ae69135e
Kenneth Bautista <kennethbautista1512@gmail.com>
2026-03-28 13:12:30 -0400

    added the configuration for the routers

---
+++ campusconnect-frontend/src/MainRouter.jsx
@@ -0,0 +1,11 @@
+//this will be all the routers will be defined here and then imported in App.jsx
+import { Route, Routes } from "react-router-dom";
+
+function MainRouter(){
+    return (
+        <>
+        </>
+    )
+}
+
+export default MainRouter;
